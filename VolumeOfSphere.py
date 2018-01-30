3bd9e8f4800dcd8ae3e60845276dfff2cefa5ede
hrd2910 <35139001+hrd2910@users.noreply.github.com>
2018-01-30 23:01:13 +0530

    Add files via upload

---
+++ VolumeOfSphere.py
@@ -0,0 +1,15 @@
+
+
+
+print("Question: Write a Python program to find the volume of a sphere with diameter 12 cm.\n")
+print("Solution")
+print("Given diameter of the sphere is = 12 cm ")
+print("So radius of the sphere is = 6 cm ")
+r=6.0
+print("Value of the constant PI is = 3.14 ")
+pi=3.14
+V= 4.0/3.0*pi* r**3
+print("Hence the volume of the sphere is: ",V)
+
+
+
